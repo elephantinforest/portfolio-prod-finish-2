d8134c725dc43485ace43015cbe74cbdce027c79
elephantinforest <sikurowaiyado@gmail.com>
2024-07-02 18:12:15 +0900

    news 11

--- src/src/migration/initialize_all_table.php
+++ src/src/migration/initialize_all_table.php
@@ -7,17 +7,17 @@ $db = [
     'password' => getenv('MYSQL_PASSWORD'),
     'database' => getenv('MYSQL_DATABASE'),
 ];
-// データベースへの接続
-$mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+
+// MySQLデータベースに接続
+$mysqli = new mysqli($db['hostname'], $db['username'], $db['password'], $db['database']);
+
+// 接続エラーの確認
 if ($mysqli->connect_error) {
-   die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-  throw new RuntimeException('mysqli接続エラー: ' . $mysqli->connect_error);
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
+
 // テーブルを作成する関数の呼び出し
 createTables($mysqli);
 
 // データベース接続を閉じる
 $mysqli->close();
-
-
-// テーブルを作成する関数
