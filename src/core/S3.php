8d2e23f52345cc661b3834c1bac2f554009bb1fb
root <root@DESKTOP-CAM7NNT>
2024-07-05 21:13:28 +0900

    test1

--- src/core/S3.php
+++ src/core/S3.php
@@ -16,7 +16,7 @@ class S3
         $this->bucket = 'portfolio-mononoke-imgs';
 
         //  $environment = getenv('ENVIRONMENT');
-         $environment = 'developnt';
+         $environment = 'development';
 
         if ($environment === 'development') {
             $credentials = [
