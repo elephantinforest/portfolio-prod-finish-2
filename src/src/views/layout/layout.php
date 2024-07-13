7332198c0a15467c095a22852885279d37d701cd
root <root@DESKTOP-CAM7NNT>
2024-07-13 16:57:40 +0900

    refucturing2

--- src/src/views/layout/layout.php
+++ src/src/views/layout/layout.php
@@ -19,6 +19,7 @@
     //
     ?>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js" />
 </head>
 
 
