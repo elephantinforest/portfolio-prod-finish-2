38e9486ca4a77f5d88b70c826fd4b63685e1b7b0
elephantinforest <sikurowaiyado@gmail.com>
2024-10-08 17:13:00 +0900

    commit

--- src/core/S3.php
+++ src/core/S3.php
@@ -34,7 +34,7 @@ class S3
         $this->bucket = 'portfolio-mononoke-imgs';
 
         // 環境変数から環境を取得
-        $environment = 'development';
+        $environment = 'deveent';
 
         // 開発環境の場合
         if ($environment == 'development') {
